03dce63c6a6019b826e3f6a8cb9d30b35beb2f4c
Aditya Kavdikar <aditya.kavdikar@yash.com>
2019-05-21 17:52:01 +0530

    ignored files

---
+++ GitRepoWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitRepoWebApplication.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
